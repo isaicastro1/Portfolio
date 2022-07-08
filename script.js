61daa4c2e032ad7a99d8fc0b94a1c068d132a637
isaicastro1 <63988161+isaicastro1@users.noreply.github.com>
2022-07-07 18:08:05 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// Goes to top of page after page refresh
+window.onbeforeunload = function () {
+  window.scrollTo(0,0);
+};
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (let i = 0; i < reveals.length; i++) {
+      let windowHeight = window.innerHeight;
+      let elementTop = reveals[i].getBoundingClientRect().top;
+      let elementVisible = 150; 
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
